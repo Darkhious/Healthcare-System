ae8e520db2a6e3e3567efc3580696df634e20e48
Darkhious <162713985+Darkhious@users.noreply.github.com>
2024-03-29 21:05:33 +0800

    Update sysmptoms3.cpp

--- Healthcare System/sysmptoms3.cpp
+++ Healthcare System/sysmptoms3.cpp
@@ -7,47 +7,65 @@ int main()
 {
     const int MAXELEMENTS = 36;
 
-    string symptoms[MAXELEMENTS] = {"Altered mental sensorium", "Abdominal cramp/pain", "Anorexia", "Bleeding gums", "Body weakness", "Blurring of vision", "Chest pain/discomfort", "Constipation", "Cough", "Diarrhea", "Dizziness", "Dysphagia", "Dyspnea", "Dysuria", "Epistaxis", "Fever", "Frequency of urination", "Headache", "Hematemesis", "Hematuria", "Hemotysis", "Irritability", "Jaundice", "Lower extremity edema", "Myalgia", "Orthopnea", "Pain(site)", "Palpitations", "Seizures", "Skin rashes", "Stool,bloody/black tarry/mucoid", "Sweating", "Urgency", "Vomiting", "Weight loss", "Others"};
+    string symptoms[MAXELEMENTS] = {"Altered mental sensorium", "Abdominal cramp/pain", "Anorexia               ", "Bleeding gums           ", "Body weakness           ", "Blurring of vision      ", "Chest pain/discomfort", "Constipation           ", "Cough               ", "Diarrhea                ", "Dizziness               ", "Dysphagia               ", "Dyspnea               ", "Dysuria               ", "Epistaxis               ", "Fever               ", "Frequency of urination", "Headache               ", "Hematemesis        ", "Hematuria           ", "Hemotysis           ", "Irritability        ", "Jaundice            ", "Lower extremity edema", "Myalgia            ", "Orthopnea           ", "Pain(site)          ", "Palpitations", "Seizures", "Skin rashes", "Stool,bloody/black tarry/mucoid", "Sweating", "Urgency", "Vomiting", "Weight loss", "Others"};
     bool chosen[MAXELEMENTS] = {false};
-    int ctr, i, accummulator, watcher;
+    int ctr, i, accummulator, watcher, choice;
     bool examining;
 
-    ctr = 1;
-    accummulator = 1;
-    watcher = 29;
-    i = 0;
     examining = true;
 
     while (examining)
     {
-        if(chosen[i])
+        ctr = 1;
+        accummulator = 1;
+        watcher = 29;
+        i = 0;
+
+        while (true)
         {
-            cout<<"[/] "<<symptoms[i]<<"\t\t";
+            if(chosen[i])
+            {
+                cout<<"[/] "<<symptoms[i]<<"\t\t\t";
+            }
+                else
+                {
+                    cout<<"["<<ctr<<"] "<<symptoms[i]<<"\t\t\t";
+                }
+
+            ctr += 9;
+            i += 9;
+
+            if (ctr >= watcher)
+            {
+                cout<<endl;
+
+                ctr = 1;
+                i = 0;
+                ctr += accummulator;
+                i += accummulator;
+                accummulator++;
+                watcher++;
+            }
+
+            if (watcher == 38)
+            {
+                break;
+            }
+        }
+
+        cout<<"\n\nSilik ol dat aplays mah boi [1/2/3...]: ";
+        cin>>choice;
+
+        choice--;
+
+        if (choice >= 0)
+        {
+            chosen[choice] = true;
         }
             else
             {
-                cout<<"["<<ctr<<"] "<<symptoms[i]<<"\t\t";
+                examining = false;
             }
-
-        ctr += 9;
-        i += 9;
-
-        if (ctr >= watcher)
-        {
-            cout<<endl;
-
-            ctr = 1;
-            i = 0;
-            ctr += accummulator;
-            i += accummulator;
-            accummulator++;
-            watcher++;
-        }
-
-        if (watcher == 38)
-        {
-            break;
-        }
     }
 
     return EXIT_SUCCESS;
